afdda6eb5f88b0bc8d79d84711facebe589bb922
Taeyoung Kim <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-07-04 13:43:37 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -5,8 +5,12 @@ using namespace std;
 int main() {
   double A, B; cin >> A >> B;
 
+  // 소수점 이하 10자리까지 표시
   cout.precision(10);
+
+  // 실수를 고정소수점 형식으로 출력(1.23e+06 X)
   cout << fixed;
+  
   cout << A / B << endl;
 
   return 0;
