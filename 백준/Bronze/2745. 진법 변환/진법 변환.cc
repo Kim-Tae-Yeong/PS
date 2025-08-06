f0dbfefad7756712f6033038dd19abf44805d822
Taeyoung Kim <108168686+Kim-Tae-Yeong@users.noreply.github.com>
2025-08-06 20:48:13 +0900

    Update 진법 변환.cc

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <cctype>
-#include <math.h>
+// bits/stdc++.h : c++에서 자주 사용하는 표준 헤더들을 모아놓음
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -16,6 +15,8 @@ int main() {
   len = N.length() - 1;
   ans = 0;
   for(auto it = N.begin(); it != N.end(); it++) {
+    // c++에서는 isdigit() 사용
+    // isnumber 함수는 없음
     num = (isdigit(*it)) ? int(*it) - 48 : int(*it) - 55;
     ans += num * pow(B, len);
     len--;
